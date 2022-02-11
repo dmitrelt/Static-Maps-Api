59829c84e019db403ea396d99a90800cd8eb738e
mishasverx <mishasverx@gmail.com>
2022-02-12 00:42:03 +0300

    вторая задача

--- main.py
+++ main.py
@@ -1,34 +1,55 @@
 import os
 import sys
 
-import pygame
+import pygame as pg
 import requests
 
-t_lon = input()
-t_lat = input()
-delta = input()
-map_server = f"http://static-maps.yandex.ru/1.x/"
-map_params = {
-    "ll": ",".join([t_lon, t_lat]),
-    "spn": ",".join([delta, delta]),
-    "l": "map"
-}
-response = requests.get(map_server, params=map_params)
+pg.init()
+screen = pg.display.set_mode((600, 450))
 
-if not response:
-    print("Ошибка выполнения запроса")
-    sys.exit(1)
 
-map_file = "map.png"
-with open(map_file, "wb") as file:
-    file.write(response.content)
+class Map:
+    def __init__(self):
+        super().__init__()
+        self.t_lon = "39.55166106940937"
+        self.t_lat = "52.604717305615324"
+        self.z = 5
 
-pygame.init()
-screen = pygame.display.set_mode((600, 450))
-screen.blit(pygame.image.load(map_file), (0, 0))
-pygame.display.flip()
-while pygame.event.wait().type != pygame.QUIT:
-    pass
-pygame.quit()
+    def get(self):
+        map_server = f"http://static-maps.yandex.ru/1.x/"
+        map_params = {
+            "ll": ",".join([self.t_lon, self.t_lat]),
+            "z": self.z,
+            "l": "map"
+        }
+        response = requests.get(map_server, params=map_params)
 
-os.remove(map_file)
+        if not response:
+            print("Ошибка выполнения запроса")
+            sys.exit(1)
+        self.map_file = "map.png"
+        with open(self.map_file, "wb") as file:
+            file.write(response.content)
+        screen.blit(pg.image.load(self.map_file), (0, 0))
+        self.set()
+
+    def set(self):
+        keys = pg.key.get_pressed()
+        if keys[pg.K_PAGEUP] and self.z <= 20:
+            self.z += 1
+        if keys[pg.K_PAGEDOWN] and self.z > 0:
+            self.z -= 1
+
+
+
+map = Map()
+running = True
+while running:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+    map.get()
+    pg.display.update()
+os.remove(map.map_file)
+pg.quit()
+sys.exit()
